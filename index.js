f38f80b878de234da1952ecb2992687b88ad3fae
Brandon Casey <brandonocasey@gmail>
2024-02-22 01:04:28 -0500

    feat(BREAKING): switch to serve-handler for directory listings

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 /* eslint-disable no-console */
-const serveStatic = require('serve-static');
+const serveHandler = require('serve-handler');
 
 const StaticServerMiddlewareFactory = function(config, logger) {
   const log = logger.create('karma-static-server');
   const options = Object.assign({
     root: config.basePath,
     // do not log during singleRun
-    log: config.singleRun ? false : true,
-    index: ['index.html', 'index.htm']
+    log: config.singleRun ? false : true
   }, (config.serveStatic || {}));
 
   if (options.log) {
     log.info(`serving static files in ${options.root}`);
   }
 
-  const serve = serveStatic(options.root, options);
-
   return function(request, response, next) {
     if (options.log) {
       response.addListener('finish', () => {
-        if (response.statusCode !== 404) {
-          log.info(`[${response.statusCode}] ${request.url}`);
-        }
+        log.info(`[${response.statusCode}] ${request.url}`);
       });
     }
-    return serve(request, response, next);
+    return serveHandler(request, response);
   };
 };
 
